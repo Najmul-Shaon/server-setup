51b361af8fbdd8f401613f883d60d5702b523d87
Najmul shaon <najmul.nh.shaon@gmail.com>
2024-11-24 23:33:28 +0600

    added

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [users, setUser] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/users")
+      .then((res) => res.json())
+      .then((data) => setUser(data));
+  }, []);
+
+
+  const handleSubmit = (e) => {
+    e.proventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.name.value;
+  }
+  return (
+    <>
+      <h3>User management : {users.length}</h3>
+      <form onSubmit={}>
+        <input type="text" />
+        <br />
+        <input type="email" />
+        <br />
+        <input type="submit" />
+      </form>
+      <div>
+        {users.map((user) => (
+          <p>{user.name}</p>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
